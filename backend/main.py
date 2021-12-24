92479ec4de07eebbc5383f7c6c1b91e852e1a8dc
hellomyzn <em711th@icloud.com>
2021-12-24 11:51:57 +0900

    fix:

--- backend/main.py
+++ backend/main.py
@@ -28,87 +28,59 @@ def get_urls() -> list:
     return urls
 
 
-def get_data_from_cambridge(url: str):
+def get_data_from_cambridge(url: str) -> dict:
     '''Get vocabulary data from cambridge'''
 
     # TO AVOID SCRAPING ERROR ON CAMBRIDGE SITE: requests.exceptions.ConnectionError: ('Connection aborted.', RemoteDisconnected('Remote end closed connection without response'))        
     # REF: https://gammasoft.jp/support/solutions-of-requests-get-failed/
     headers_dic = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.97 Safari/537.36"}
 
-    vocabularies = []
-    parts_of_speechs = []
-    meanings = []
-    example_sentences = []
-
     html = requests.get(url, headers=headers_dic)
     soup = bs4.BeautifulSoup(html.content, "html.parser")
 
-    # Get data of pronunciation
-    us_pronunciation = soup.select('.us > .pron > .ipa', limit=1)[0].text 
-    uk_pronunciation = soup.select('.uk > .pron > .ipa', limit=1)[0].text
-    
-    # Get all difinition
-    definitions = [d for d in soup.find_all(class_="pr entry-body__el")]
-    for defi in definitions:
-        vocabularies.append(defi.find(class_="hw dhw").text)
-        parts_of_speechs.append(defi.find(class_="pos dpos").text)
+    # Set class for scraping
+    s_title = '.di-title .hw'
+    s_parts_of_speech = '.pos'
+    s_us_pronunciation = '.us > .pron > .ipa'
+    s_uk_pronunciation = '.uk > .pron > .ipa'
+    s_definition = '.ddef_h > .def'
+    s_example = '.ddef_b > .examp > .eg'
 
-        # Get the block of details 
-        details_of_definitions = [d for d in defi.find_all(class_="def-block ddef_block")]
-        meaning = []
-        example_sentence = []
+    # Get data via scraping
+    vocabulary = {}
+    vocabulary['title']       = soup.select(s_title, limit=1)[0].text
+    vocabulary['parts_of_speechs'] = soup.select(s_parts_of_speech, limit=1)[0].text
+    vocabulary['us_pronunciation'] = soup.select(s_us_pronunciation, limit=1)[0].text 
+    vocabulary['uk_pronunciation'] = soup.select(s_uk_pronunciation, limit=1)[0].text    
+    vocabulary['definition']       = soup.select(s_definition, limit=1)[0].text
+    vocabulary['example_sentence'] = soup.select(s_example, limit=1)[0].text
 
-        for dod in details_of_definitions:
-            print("#############################################################")
-            meaning += [d.find(class_="ddef_d").text for d in dod.select(".ddef_h", limit=1)]
-            # Get the first example sentence 
-            print(dod)
-            example_sentence += [d.find(class_='eg').text for d in dod.select(".ddef_b > .examp", limit=1)]
-            
-        
-        meanings.append(meaning)
-        example_sentences.append(example_sentence)
-
-    return vocabularies, parts_of_speechs, us_pronunciation, uk_pronunciation, meanings, example_sentences
+    return vocabulary
 
 
-def write_csv(vocabularies: list, 
-                parts_of_speechs: list, 
-                us_pronunciation: str, 
-                uk_pronunciation: str, 
-                meanings: list, 
-                example_sentences: list):
+def write_csv(vocabulary: dict):
     '''Write vacabularies on CSV(/backend/data/vocabularies.csv)'''
 
     with open('data/vocabularies.csv', 'w', newline='') as csvfile:
-        fieldnames = ['vocabulary', 'parts of speach', 'us pronunciation', 'uk pronunciation', 'definition', 'example sentence']
+        fieldnames = ['title', 'parts of speach', 'us pronunciation', 'uk pronunciation', 'definition', 'example sentence']
         writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
-
         writer.writeheader()
-        for i in range(len(vocabularies)):
-            # Debug1: match number of meanings and number of example
-            print('DEBUG1: ', len(meanings[i]), ' : ',  len(example_sentences[i]))
-            for j in range(len((meanings[i]))):
-                # Debug2: 
-                print('DEBUG2: ', 'Meaning: ', meanings[i][j])
-                print('DEBUG2: ', 'Example: ', example_sentences[i][j])
-                # print(meanings[i][j], ' : ', example_sentences[i][j])
-                writer.writerow({'vocabulary': vocabularies[i], 
-                                'parts of speach': parts_of_speechs[i], 
-                                'us pronunciation': us_pronunciation, 
-                                'uk pronunciation': uk_pronunciation, 
-                                'definition': meanings[i][j], 
-                                'example sentence': example_sentences[i][j]})
+        
+        writer.writerow({'title': vocabulary['title'],
+                        'parts of speach': vocabulary['parts_of_speechs'],
+                        'us pronunciation': vocabulary['us_pronunciation'],
+                        'uk pronunciation': vocabulary['uk_pronunciation'],
+                        'definition': vocabulary['definition'],
+                        'example sentence': vocabulary['example_sentence']})
 
 
 def main():
     # urls = get_urls()
     urls = ['https://dictionary.cambridge.org/dictionary/english/make']
     for url in urls:
-        print('\n\n#####################################################################################')
-        print(url)
-        vocabularies, parts_of_speechs, us_pronunciation, uk_pronunciation, meanings, example_sentences = get_data_from_cambridge(url)
-        write_csv(vocabularies, parts_of_speechs, us_pronunciation, uk_pronunciation, meanings, example_sentences)
+        
+        vocabulary = get_data_from_cambridge(url)
+        write_csv(vocabulary)
 
 if __name__ == "__main__":
     main()
