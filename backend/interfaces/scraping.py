57371967ec1d9362d03022dd7fdb468b37111a93
hellomyzn <em711th@icloud.com>
2022-03-17 12:18:26 +0900

    add: japanese example if it's weblio

--- backend/interfaces/scraping.py
+++ backend/interfaces/scraping.py
@@ -20,7 +20,6 @@ class Scraping(object):
 
     def get_vocabulary(self, vocabulary: Vocabulary, soup) -> Vocabulary:
         # Get data via scraping
-        print(vocabulary)
         vocabulary.title            = soup.select(self.title,            limit=1)[0].text if soup.select(self.title, limit=1) else self.substitution_title
         vocabulary.part_of_speech   = soup.select(self.parts_of_speech,  limit=1)[0].text if soup.select(self.parts_of_speech, limit=1) else ''
         vocabulary.us_pronunciation = soup.select(self.us_pronunciation, limit=1)[0].text if soup.select(self.us_pronunciation, limit=1) else ''
--- backend/models/weblio.py
+++ backend/models/weblio.py
@@ -12,20 +12,22 @@ class Weblio(scraping.Scraping):
         self.us_pronunciation = '.phoneticEjjeDesc'
         self.uk_pronunciation = '.phoneticEjjeDesc'
         self.definition = '.content-explanation'
-        self.example = '.KejjeYrEn'
+        self.example_en = '.KejjeYrEn'
+        self.example_jp = '.KejjeYrJp'
         self.url_for_search = 'https://ejje.weblio.jp/content/'
 
 
     def get_vocabulary(self, vocabulary: Vocabulary, soup) -> Vocabulary:
         # Get data via scraping
         print(vocabulary)
-        vocabulary.title            = soup.select(self.title,            limit=1)[0].text if soup.select(self.title, limit=1) else self.substitution_title
-        vocabulary.part_of_speech   = soup.select(self.parts_of_speech,  limit=1)[0].text if soup.select(self.parts_of_speech, limit=1) else ''
-        vocabulary.us_pronunciation = soup.select(self.us_pronunciation,        )[0].text if soup.select(self.us_pronunciation, limit=1) else ''
-        vocabulary.uk_pronunciation = soup.select(self.uk_pronunciation,        )[1].text if soup.select(self.uk_pronunciation, limit=1) else ''
-        vocabulary.definition       = soup.select(self.definition,       limit=1)[0].text if soup.select(self.definition, limit=1) else ''
-        vocabulary.definition       = soup.select(self.definition,       limit=1)[0].text if soup.select(self.definition, limit=1) else ''
-        vocabulary.example_sentence = soup.select(self.example,          limit=1)[0].text if soup.select(self.example, limit=1) else ''
+        vocabulary.title                = soup.select(self.title,            limit=1)[0].text if soup.select(self.title, limit=1) else self.substitution_title
+        vocabulary.part_of_speech       = soup.select(self.parts_of_speech,  limit=1)[0].text if soup.select(self.parts_of_speech, limit=1) else ''
+        vocabulary.us_pronunciation     = soup.select(self.us_pronunciation,        )[0].text if soup.select(self.us_pronunciation, limit=1) else ''
+        vocabulary.uk_pronunciation     = soup.select(self.uk_pronunciation,        )[1].text if soup.select(self.uk_pronunciation, limit=1) else ''
+        vocabulary.definition           = soup.select(self.definition,       limit=1)[0].text if soup.select(self.definition, limit=1) else ''
+        vocabulary.example_sentence_en  = soup.select(self.example_en,          limit=1)[0].text if soup.select(self.example_en, limit=1) else ''
+        vocabulary.example_sentence_jp  = soup.select(self.example_jp,          limit=1)[0].text if soup.select(self.example_jp, limit=1) else ''
+        vocabulary.example_sentence = vocabulary.example_sentence_en + ': ' + vocabulary.example_sentence_jp
 
         # Remove unnecessary blank
         vocabulary.definition = vocabulary.definition.lstrip()
